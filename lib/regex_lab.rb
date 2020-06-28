ddea9e80f799ea33ccfc1fc883576eae2a2a0320
Mark May <mamark90@gmail.com>
2020-06-28 17:59:18 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,16 @@
 require 'pry'
+
 def starts_with_a_vowel?(word)
-  #binding.pry
-  word.match(/^[AaEeIiOoUu]/) ? true : false
+  if word.match(/\b[AEIOUaeiou]/)
+    return true
+  elsif word.match(/\b[^\sAEIOUaeiou]/)
+    #binding.pry
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  #binding.pry
-  text.scan(/\w+ing/)
-
+  text.scan(/\bun\w+ing/)
 end
 
 def words_five_letters_long(text)
@@ -15,10 +18,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/[A-Z.?!] [a-z]/) ? true : false
-
+  text.match?(/^[A-Z].*\W$/)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/\d{3}\d{3}\d{4}/) ? true : false
+  phone.match?(/\(?\d{3}[) ]?\d{3}[- ]?\d{4}$/)
 end
